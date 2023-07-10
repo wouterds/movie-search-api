c1db440c1b92fb6a6201caf44cfc4df8423d2177
Wouter De Schuyter <wouter.de.schuyter@gmail.com>
2023-07-10 12:50:47 +0200

    fix(tmdb): could be null

--- src/services/tmdb/utils.ts
+++ src/services/tmdb/utils.ts
@@ -4,8 +4,12 @@ import { TMDBMovie } from './types';
 export const normalizeMovie = (movie: TMDBMovie) => ({
   id: Obfuscator.encode(movie.id),
   title: movie.title,
-  poster: `/movies/images/${movie.poster_path}`.replace('//', '/'),
-  wallpaper: `/movies/images/${movie.backdrop_path}`.replace('//', '/'),
+  poster: movie.poster_path
+    ? `/movies/images/${movie.poster_path}`.replace('//', '/')
+    : null,
+  wallpaper: movie.backdrop_path
+    ? `/movies/images/${movie.backdrop_path}`.replace('//', '/')
+    : null,
   description: movie.overview,
   releaseDate: new Date(movie.release_date),
 });
